50c805bab0422288b32003c8eea4a02cd71d3a40
Aayush shah <23053738@kiit.ac.in>
2026-02-21 16:23:38 +0530

    Added the cookiecutter templet

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='mini-project to predeict the student performance in the next exam',
+    author='Aayush_shah',
+    license='',
+)
